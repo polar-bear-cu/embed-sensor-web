3e5b548ffcf00b64a2e23686bd71a41e7d14a595
BadLuckZ <thanagorn8802@gmail.com>
2025-12-02 13:01:08 +0700

    chore: init project

---
+++ frontend/src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import FetchData from './components/FetchData.vue'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+    <HelloWorld msg="Hello World!" />
+  </header>
+
+  <body>
+    <FetchData />
+  </body>
+
+  <main></main>
+</template>
